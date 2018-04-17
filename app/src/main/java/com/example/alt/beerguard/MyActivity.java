c8e2893275887e6c3c324ce02ae68336295c0390
hannay1 <halmohanna.HAM@gmail.com>
2018-04-17 18:15:52 -0500

    messed up

--- app/src/main/java/com/example/alt/beerguard/MyActivity.java
+++ app/src/main/java/com/example/alt/beerguard/MyActivity.java
@@ -6,8 +6,12 @@ import android.content.ComponentName;
 import android.content.Context;
 import android.content.Intent;
 import android.content.ServiceConnection;
+import android.os.Build;
 import android.os.Bundle;
 import android.os.IBinder;
+import android.os.VibrationEffect;
+import android.os.Vibrator;
+import android.support.annotation.RequiresApi;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 import android.view.View;
@@ -26,16 +30,32 @@ import com.mbientlab.metawear.builder.function.Function1;
 import com.mbientlab.metawear.data.Acceleration;
 import com.mbientlab.metawear.module.Accelerometer;
 import com.mbientlab.metawear.module.Logging;
+import com.mbientlab.metawear.module.Temperature;
+
+
+
+
+/*
+create activity intent
+ */
+
+import java.util.concurrent.Executors;
+import java.util.concurrent.ScheduledExecutorService;
+import java.util.concurrent.TimeUnit;
 
 import bolts.Continuation;
 import bolts.Task;
-
 public class MyActivity extends AppCompatActivity implements ServiceConnection {
 
     private BtleService.LocalBinder serviceBinder;
     private MetaWearBoard board;
     private Accelerometer accelerometer;
-    private Logging logging;
+    private Runnable run_temp = new Runnable() {
+        @Override
+        public void run() {
+            readTemp();
+        }
+    };
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -45,36 +65,27 @@ public class MyActivity extends AppCompatActivity implements ServiceConnection {
         // Bind the service when the activity is created
         getApplicationContext().bindService(new Intent(this, BtleService.class),
                 this, Context.BIND_AUTO_CREATE);
+
         findViewById(R.id.start).setOnClickListener(new View.OnClickListener() {
 
             @Override
             public void onClick(View v) {
-                Log.i("freefall", "start");
-                logging.start(false);
+                Log.i("beerguard", "start");
                 accelerometer.acceleration().start();
                 accelerometer.start();
+                ScheduledExecutorService read_temp = Executors.newScheduledThreadPool(1);
+                read_temp.scheduleAtFixedRate(run_temp, 0, 1, TimeUnit.MINUTES);
+
+
+
             }
         });
         findViewById(R.id.stop).setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Log.i("freefall", "stop");
+                Log.i("beerguard", "stop");
                 accelerometer.stop();
                 accelerometer.acceleration().stop();
-                logging.stop();
-                logging.downloadAsync().continueWith(new Continuation<Void, Void>() {
-                    @Override
-                    public Void then(Task<Void> task) throws Exception {
-                        if (task.isFaulted()){
-                            Log.w("freefall", "Logging download failed: " + task.getError());
-                        }else
-                        {
-                            Log.i("freefall", "Logging successful");
-                        }
-                        return null;
-                    }
-                });
-
             }
         });
         findViewById(R.id.reset).setOnClickListener(new View.OnClickListener() {
@@ -97,8 +108,9 @@ public class MyActivity extends AppCompatActivity implements ServiceConnection {
     @Override
     public void onServiceConnected(ComponentName name, IBinder service) {
         serviceBinder = (BtleService.LocalBinder) service;
-        Log.i("freefall", "Service Connected");
-        retrieveBoard("PUT YOUR BOARD MAC ADDRESS HERE");
+        Log.i("beerguard", "Service Connected");
+        retrieveBoard("F1:51:4D:F3:B9:AC");
+
 
     }
 
@@ -106,6 +118,7 @@ public class MyActivity extends AppCompatActivity implements ServiceConnection {
     public void onServiceDisconnected(ComponentName name) {
 
     }
+
 
     private void retrieveBoard(String mac_addr) {
         final BluetoothManager btManager=
@@ -118,9 +131,8 @@ public class MyActivity extends AppCompatActivity implements ServiceConnection {
         board.connectAsync().onSuccessTask(new Continuation<Void, Task<Route>>() {
             @Override
             public Task<Route> then(Task<Void> task) throws Exception {
-                Log.i("freefall", "Connected to board with " + mac_addr);
+                Log.i("beerguard", "Connected to board with " + mac_addr);
 
-                logging = board.getModule(Logging.class);
                 accelerometer = board.getModule(Accelerometer.class);
                 accelerometer.configure()
                         .odr(60f)       // Set sampling frequency to 25Hz, or closest valid ODR
@@ -128,31 +140,70 @@ public class MyActivity extends AppCompatActivity implements ServiceConnection {
                 return accelerometer.acceleration().addRouteAsync(new RouteBuilder() {
                     @Override
                     public void configure(RouteComponent source) {
-                        source.map(Function1.RSS).average((byte) 4).filter(ThresholdOutput.BINARY, 0.5f).multicast().to().filter(Comparison.EQ, -1).log(new Subscriber() {
+                        source.map(Function1.RSS).average((byte) 4).filter(ThresholdOutput.BINARY, 0.80f).multicast().to().filter(Comparison.EQ, -1).stream(new Subscriber() {
                             @Override
                             public void apply(Data data, Object... env) {
-                                Log.i("freefall", data.formattedTimestamp() + ": in freefall");
+                                Log.i("beerguard", "theft");
                             }
-                        }).to().filter(Comparison.EQ, 1).log(new Subscriber() {
+                        }).to().filter(Comparison.EQ, 1).stream(new Subscriber() {
                             @Override
                             public void apply(Data data, Object... env) {
-                                Log.i("freefall", data.formattedTimestamp() + ": no freefall");
+                                Log.i("beerguard", "no theft");
                             }
                         }).end();
                     }
                 });
+
+
             }
         }).continueWith(new Continuation<Route, Void>() {
             @Override
             public Void then(Task<Route> task) throws Exception {
                 if (task.isFaulted()){
-                    Log.w("freefall", "Failed to configure app, " + task.getError());
+                    Log.w("beerguard", "Failed to configure app, " + task.getError());
                 }else
                 {
-                    Log.i("freefall", "App successfully configured");
+                    Log.i("beerguard", "App successfully configured");
                 }
                 return null;
             }
         });
+
     }
+
+    private void readTemp()
+    {
+        final Temperature temperature = board.getModule(Temperature.class);
+        final Temperature.Sensor temp_sensor = temperature.findSensors(Temperature.SensorType.PRESET_THERMISTOR)[0];
+        final Vibrator vibe = (Vibrator) getSystemService(Context.VIBRATOR_SERVICE);
+
+        temp_sensor.addRouteAsync(new RouteBuilder() {
+            @Override
+            public void configure(RouteComponent source) {
+                source.stream(new Subscriber() {
+                    @Override
+                    public void apply(Data data, Object... env) {
+                        Log.i("beerguard", "Temperature (C) = " +  data.value(Float.class));
+                        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O)
+                        {
+                            if(data.value(Float.class) < 25.0)
+                            {
+                                vibe.vibrate(VibrationEffect.createOneShot(500, VibrationEffect.DEFAULT_AMPLITUDE));
+                            }
+                        }
+
+                    }
+                });
+            }
+        }).continueWith(new Continuation<Route, Void>() {
+
+            @Override
+            public Void then(Task<Route> task) throws Exception {
+                temp_sensor.read();
+                return null;
+            }
+        });
+
+    }
+
 }
